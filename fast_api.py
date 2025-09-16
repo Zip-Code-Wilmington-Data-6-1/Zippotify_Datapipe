8b0362bd63f3dccff979799e54fa03c38db1c6c4
skanamar017 <skanamar017@gmail.com>
2025-09-16 14:29:10 -0400

    added some api calls

--- fast_api.py
+++ fast_api.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Depends
 from typing import List
 from sqlalchemy.orm import Session
 from database import SessionLocal
-from models import DimUser
+from models import DimArtist, DimLocation, DimUser
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -16,6 +16,25 @@ class User(BaseModel):
 
     class Config:
         orm_mode = True
+
+class Artist(BaseModel):
+    artist_id: int
+    artist_name: str
+
+    class Config:
+        orm_mode = True
+
+class location(BaseModel):
+    location_id: int
+    city: str
+    state: str
+    latitude: float
+    longitude: float
+
+    class Config:
+        orm_mode = True
+
+
 
 def get_db():
     db = SessionLocal()
@@ -33,5 +52,30 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
     user = db.query(DimUser).filter(DimUser.user_id == user_id).first()
     if user:
         return user
+    else:
+        return {}
+
+@app.get("/artists", response_model=List[Artist])
+def get_artists(db: Session = Depends(get_db)):
+    return db.query(DimArtist).all()
+
+@app.get("/artists/{artist_id}", response_model=Artist)
+def get_artist(artist_id: int, db: Session = Depends(get_db)):
+    artist = db.query(DimArtist).filter(DimArtist.artist_id == artist_id).first()
+    if artist:
+        return artist
+    else:
+        return {}  
+
+@app.get("/locations", response_model=List[location])
+def get_locations(db: Session = Depends(get_db)):
+    return db.query(DimLocation).all()
+
+
+@app.get("/locations/{location_id}", response_model=location)
+def get_location(location_id: int, db: Session = Depends(get_db)):
+    location = db.query(DimLocation).filter(DimLocation.location_id == location_id).first()
+    if location:
+        return location
     else:
         return {}
